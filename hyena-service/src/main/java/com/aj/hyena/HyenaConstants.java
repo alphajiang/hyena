dcc7301b41a08c97a79522904c1bdbca4c435a57
Alpha Jiang <jiangjianzheng@parkmecn.com>
2019-03-31 13:58:53 +0800

    exception handler

--- hyena-service/src/main/java/com/aj/hyena/HyenaConstants.java
+++ hyena-service/src/main/java/com/aj/hyena/HyenaConstants.java
@@ -25,6 +25,7 @@ public interface HyenaConstants {
     int RES_CODE_NO_ENOUGH_POINT = 1010;
     int RES_CODE_PARAMETER_ERROR = 1100;
     int RES_CODE_STATUS_ERROR = 1200;
+    int RES_CODE_SERVER_ERROR = 9000;
     int RES_CODE_UNKNOW_ERROR = 9999;
 
     String PREFIX_POINT_TABLE_NAME = "t_point_";
