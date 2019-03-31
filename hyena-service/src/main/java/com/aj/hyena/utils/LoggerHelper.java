4eb6428f9f75b47cced3a09aa8ed90799a4b73ac
Alpha Jiang <jiangjianzheng@parkmecn.com>
2019-03-31 11:50:02 +0800

    apache v2 copyright

--- hyena-service/src/main/java/com/aj/hyena/utils/LoggerHelper.java
+++ hyena-service/src/main/java/com/aj/hyena/utils/LoggerHelper.java
@@ -1,3 +1,20 @@
+/*
+ *  Copyright (C) 2019 Alpha Jiang. All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
 package com.aj.hyena.utils;
 
 import javax.servlet.http.HttpServletRequest;
@@ -7,54 +24,54 @@ import java.util.Iterator;
 public class LoggerHelper {
 
 
-	public static String formatEnterLog(HttpServletRequest request) {
-		return ">> " + LoggerHelper.getUserLog(request, true);
-	}
+    public static String formatEnterLog(HttpServletRequest request) {
+        return ">> " + LoggerHelper.getUserLog(request, true);
+    }
 
-	public static String formatEnterLog(HttpServletRequest request, boolean logParams) {
-		return ">> " + LoggerHelper.getUserLog(request, logParams);
-	}
+    public static String formatEnterLog(HttpServletRequest request, boolean logParams) {
+        return ">> " + LoggerHelper.getUserLog(request, logParams);
+    }
 
-	public static String formatLeaveLog(HttpServletRequest request) {
-		return "<< " + LoggerHelper.getUserLog(request, false);
-	}
+    public static String formatLeaveLog(HttpServletRequest request) {
+        return "<< " + LoggerHelper.getUserLog(request, false);
+    }
 
-	private static String getUserLog(HttpServletRequest request, boolean logParams) {
+    private static String getUserLog(HttpServletRequest request, boolean logParams) {
 
-		StringBuilder sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
 
 
-		if (logParams) {
-			sb.append("params = { ");
-			boolean firstKey = true;
-			Iterator<String> iter = request.getParameterMap().keySet().iterator();
-			while (iter.hasNext()) {
-				String k = iter.next();
+        if (logParams) {
+            sb.append("params = { ");
+            boolean firstKey = true;
+            Iterator<String> iter = request.getParameterMap().keySet().iterator();
+            while (iter.hasNext()) {
+                String k = iter.next();
 
-				if (firstKey) {
-					firstKey = false;
-				} else {
-					sb.append(", ");
-				}
+                if (firstKey) {
+                    firstKey = false;
+                } else {
+                    sb.append(", ");
+                }
 
-				sb.append(k).append("=[");
-				boolean firstValue = true;
-				for (String v : request.getParameterMap().get(k)) {
-					if (firstValue) {
-						firstValue = false;
-					} else {
-						sb.append(",");
-					}
-					sb.append(v);
-				}
-				sb.append("]");
-			}
-			sb.append(" } ");
-			// sb.append(MessageFormat.format("params = {0}",
-			// req.getParameterMap()));
+                sb.append(k).append("=[");
+                boolean firstValue = true;
+                for (String v : request.getParameterMap().get(k)) {
+                    if (firstValue) {
+                        firstValue = false;
+                    } else {
+                        sb.append(",");
+                    }
+                    sb.append(v);
+                }
+                sb.append("]");
+            }
+            sb.append(" } ");
+            // sb.append(MessageFormat.format("params = {0}",
+            // req.getParameterMap()));
 
-		}
-		return sb.toString();
+        }
+        return sb.toString();
 
-	}
+    }
 }
