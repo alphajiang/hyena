9c073e909f55de3c2799e0b8247024e92f2e917d
Alpha <alphajiang@163.com>
2022-07-20 20:52:57 +0800

    问题修正

--- hyena-core/src/main/java/io/github/alphajiang/hyena/model/po/PointPo.java
+++ hyena-core/src/main/java/io/github/alphajiang/hyena/model/po/PointPo.java
@@ -28,7 +28,6 @@ import lombok.experimental.Accessors;
 
 @Data
 @Accessors(chain = true)
-@ToString(callSuper = true)
 @EqualsAndHashCode(callSuper = true)
 public class PointPo extends BasePo<Long> {
 
